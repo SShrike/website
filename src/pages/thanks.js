8c49c2d75e98e43d470bc3ac6b3cca550d172f5d
Severen Redwood <severen@shrike.me>
2017-10-30 02:30:36 +1300

    Add a contact form

---
+++ src/pages/thanks.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ThanksPage = () => (
+  <div>
+    <h1>Thanks!</h1>
+    <p>Your message should now be sitting in my inbox.</p>
+  </div>
+);
+
+export default ThanksPage;
