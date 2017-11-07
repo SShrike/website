5dab056a5b04f9fe1132db218de356c687690700
Severen Redwood <severen@shrike.me>
2017-11-08 12:32:49 +1300

    Add a favicon

--- src/html.js
+++ src/html.js
@@ -35,6 +35,8 @@ const HTML = ({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="hsl(94, 30%, 95%)" />
 
+        <link rel="icon" type="image/png" href="favicon.png" />
+
         {headComponents}
 
         {css}
